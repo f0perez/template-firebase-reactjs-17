0bf04cab74772da74e8f0967f93044aaae096ebb
f0.perezcanales <f0.perezcanales@gmail.com>
2023-07-22 16:26:54 -0400

    Add firebase to SignInSide

--- src/templates/SignInSide.jsx
+++ src/templates/SignInSide.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,6 +12,12 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword 
+} from 'firebase/auth';
+
+import { FirebaseAuth } from './../config/firebaseConfig';
 
 function Copyright(props) {
   return (
@@ -30,14 +36,90 @@ function Copyright(props) {
 
 const defaultTheme = createTheme();
 
-export default function SignInSide() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+export default function SignInSide({
+  //handleClose,
+  //getGroupsAndData
+}) {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorEmail, setErrorEmail] = useState(null);
+  const [errorPass, setErrorPass] = useState(null);
+  const [checked, setChecked] = useState(true);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
+  const handleLogin = async () => {
+    try {
+      setErrorEmail(null);
+      setErrorPass(null);
+      const resp = await signInWithEmailAndPassword(FirebaseAuth, email, password);
+      if (resp.user) {
+        localStorage.setItem('user', JSON.stringify(resp.user));
+        //handleClose()
+        //getGroupsAndData()
+      }
+    } catch (e) {
+      switch (e.code) {
+        case 'auth/wrong-password':
+          setErrorPass('La contraseña es incorrecta');
+          break;
+        case 'auth/user-not-found':
+          setErrorEmail('Correo no registrado');
+          break;
+        case 'auth/invalid-email':
+          setErrorEmail('Correo inválido');
+          break;
+        case 'auth/missing-password':
+          setErrorPass('Contraseña requerida');
+          break;
+        case 'auth/too-many-requests':
+          setErrorPass('El acceso a esta cuenta se ha inhabilitado temporalmente debido a muchos intentos fallidos de inicio de sesión');
+          break;
+        case 'auth/email-already-in-use':
+          setErrorPass('Este correo ya esta en uso');
+        default:
+          setErrorPass(e.message);
+          break;
+      }
+    }
+  };
+
+  const createAccount = async () => {
+    try {
+      setErrorEmail(null);
+      setErrorPass(null);
+      const resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+      if (resp.user) {
+        localStorage.setItem('user', JSON.stringify(resp.user));
+        handleClose()
+      }
+    } catch (e) {
+      switch (e.code) {
+        case 'auth/wrong-password':
+          setErrorPass('La contraseña es incorrecta');
+          break;
+        case 'auth/user-not-found':
+          setErrorEmail('Correo no registrado');
+          break;
+        case 'auth/invalid-email':
+          setErrorEmail('Correo inválido');
+          break;
+        case 'auth/missing-password':
+          setErrorPass('Contraseña requerida');
+          break;
+        case 'auth/too-many-requests':
+          setErrorPass('El acceso a esta cuenta se ha inhabilitado temporalmente debido a muchos intentos fallidos de inicio de sesión');
+          break;
+        case 'auth/email-already-in-use':
+          setErrorPass('Este correo ya esta en uso');
+        default:
+          setErrorPass(e.message);
+          break;
+      }
+    }
   };
 
   return (
@@ -72,53 +154,62 @@ export default function SignInSide() {
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+              {checked ? 'Ingresar a tu cuenta' : 'Registro de cuentas nuevas'}
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box sx={{ mt: 1 }}>
               <TextField
                 margin="normal"
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                label="Correo electrónico"
                 name="email"
                 autoComplete="email"
                 autoFocus
+                placeholder="Correo electrónico"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={errorEmail !== null}
+                helperText={errorEmail || 'Campo requerido'}
               />
               <TextField
                 margin="normal"
                 required
                 fullWidth
                 name="password"
-                label="Password"
+                label="Contraseña"
                 type="password"
                 id="password"
-                autoComplete="current-password"
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                error={errorPass !== null}
+                helperText={errorPass || (checked ? 'Campo requerido' : 'Cree una contraseña de al menos 6 caracteres')}
               />
               <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
               />
+
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
+                color={checked ? 'secondary' : 'primary'}
+                onClick={checked ? handleLogin : createAccount}
               >
-                Sign In
+                {checked ? 'Iniciar sesión' : 'Crear nueva cuenta'}
               </Button>
+
               <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
+                <Grid item style={{ width: '100%' }} sx={{ mt: 2 }}>
+                  <Link onClick={handleChange} variant="body2">
+                    {checked ? '¿No tienes una cuenta? Inscribirse' : '¿Ya tienes una cuenta? Autenticarse'}
                   </Link>
                 </Grid>
               </Grid>
+
               <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
